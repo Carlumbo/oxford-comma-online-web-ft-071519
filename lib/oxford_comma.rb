3d00d9019edc4f5d19df792fc4995ce4ccba6d9a
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-16 18:56:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -21,3 +21,10 @@ def oxford_comma(array)
   count =  0 
   if count < 2
     array.join 
+  elsif count == 2
+  array.join(" and ")
+  elsif count >= 3 
+  array[-1].insert(0," and ")
+end 
+array.join(", ")
+end 
