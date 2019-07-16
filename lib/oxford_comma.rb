d0d03a67aad7448a9bebe3237e06c4a9a69ecaae
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-16 18:52:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,8 +12,8 @@ end
 def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
-  elsif array.length > 2 
-  array[-1].insert(array.join(", "), " and ")
+ elsif 2 < array.length
+    array[-1].insert(0, "and ")
   elsif array.length < 2 
   array.join
   end 
