1888a373c147bd0bc8b908e64bdfdd26035a11fe
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-16 18:30:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,10 +8,11 @@ end
 
 def oxford_comma(array)
   count = 0
-  if count < array.length do 
+  if count <array.length do 
     array.join(", ")
   end
   else
     array.join(" and ")
+    count += 1
   end 
 end 
