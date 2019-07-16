58079cd56608a9a805e9e596781a7da2d5f7c158
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-16 18:39:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,5 +12,8 @@ end
 def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
-  else 
+  elsif array.length > 2
+  array.join(" and ")
+  elsif array.length < 2 
+  array.join
 end 
